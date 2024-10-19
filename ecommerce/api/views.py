9a65d20d3c3f37240567f3b2724332819d9cae2e
James Young <jpblicer@gmail.com>
2024-10-19 23:33:09 +0900

    add api path

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from ecommerce.api.serializers import ItemSerializer
+from .models import Item
 
 # Create your views here.
+
+class ItemListCreate(generics.ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
