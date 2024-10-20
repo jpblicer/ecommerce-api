f49c12094cea31984ea0b91737cd80efa493d80a
James Young <jpblicer@gmail.com>
2024-10-20 13:58:29 +0900

    create Cart Seralizer and CartView

--- ecommerce/api/serializers.py
+++ ecommerce/api/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from .models import Item
+from .models import CartItem, Item
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ["id", "name", "price", "quantity"]
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['item', 'quantity']
+
+    def validate_quantity(self, data):
+        item = data['item']
+        quantity = data['quantity']
+
+        if quantity >= item.quantity:
+            raise serializers.ValidationError("{} exceeds remaining stock of {}.".format(item.name, item.quantity))
+        
+        return data
--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -1,10 +1,26 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets, status
+from rest_framework.response import Response
 
-from .serializers import ItemSerializer
-from .models import Item
+from .serializers import ItemSerializer, CartSerializer
+from .models import CartItem, Item, Cart
 
 # Create your views here.
 
 class ItemListCreate(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+class CartViewSet(viewsets.ViewSet):
+    def create(self, request):
+        serializer = CartSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def list(self, request):
+        cart = request.user.cart
+        queryset = Cart.objects.filter(cart=cart)
+        serializer = CartSerializer(queryset, many=True)
+        return Response(serializer.data)
