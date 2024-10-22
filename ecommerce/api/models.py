6dd25f5a5dce9c4e41900883b72657a10d121bc1
James Young <jpblicer@gmail.com>
2024-10-22 21:50:20 +0900

    Create model for purchase records

--- ecommerce/api/models.py
+++ ecommerce/api/models.py
@@ -21,3 +21,11 @@ class CartItem(models.Model):
 
     class Meta:
         unique_together = ('cart', 'item')
+
+class PurchaseRecord(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} of {} purchased on {}".format(self.quantity, self.item.name, self.timestamp)
