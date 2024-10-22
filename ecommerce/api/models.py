ac80a8e2fd1d8d393b71682d00b94e3a26d6c873
James Young <jpblicer@gmail.com>
2024-10-22 14:11:07 +0900

    Clean up and add comments in models

--- ecommerce/api/models.py
+++ ecommerce/api/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Item(models.Model):
     name = models.CharField(max_length = 100)
     price = models.IntegerField()
     quantity = models.PositiveIntegerField(default=0)
 
+# Price is assumed to be in Yen without decimals.
+
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     items = models.ManyToManyField(Item, through='CartItem')
+
+# For now there is authenticated user. Just allow for anonymous user
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
